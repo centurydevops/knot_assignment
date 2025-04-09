f60667c141ad6d0f4b9381c5009bec440515f3b5
centurydevops <europebase5@gmail.com>
2025-04-09 16:44:50 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,20 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
 
+variable "ami_id" {
+  description = "Ubuntu AMI ID"
+  default     = "ami-0c02fb55956c7d316" # Ubuntu 22.04 LTS in us-east-1
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Name of your AWS EC2 key pair"
+}
+
+variable "allowed_ssh_ip" {
+  description = "CIDR block allowed to SSH"
+}
