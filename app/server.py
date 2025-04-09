1b879572790cf0ed089369adb0cb29fff8700a32
centurydevops <europebase5@gmail.com>
2025-04-09 16:45:31 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -1,1 +1,15 @@
 
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    if random.random() < 0.2:
+        app.logger.error("Returning 500 error")
+        return "Internal Server Error", 500
+    return "Hello, World!", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
